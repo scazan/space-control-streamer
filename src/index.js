4076b9438e5b60c5bf2a3493abeba6c209e4ba7f
scazan <scottcazan@gmail.com>
2019-06-18 17:38:29 +0200

    Adding in some extra streams and proper IPs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 const Stream = require('node-rtsp-stream')
 
-stream = new Stream({
+const stream1 = new Stream({
+  name: 'name',
+  streamUrl: 'rtsp://192.168.0.40/video.pro2',
+  // streamUrl: 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov',
+  wsPort: 9999,
+  ffmpegOptions: { // options ffmpeg flags
+    '-stats': '', // an option with no neccessary value uses a blank string
+    '-r': 30 // options with required values specify the value after the key
+  }
+});
+
+const stream2 = new Stream({
+  name: 'name',
+  streamUrl: 'rtsp://192.168.0.43/video.pro2',
+  // streamUrl: 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov',
+  wsPort: 9998,
+  ffmpegOptions: { // options ffmpeg flags
+    '-stats': '', // an option with no neccessary value uses a blank string
+    '-r': 30 // options with required values specify the value after the key
+  }
+});
+
+const stream3 = new Stream({
   name: 'name',
   streamUrl: 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov',
-  wsPort: 9999,
+  wsPort: 9997,
   ffmpegOptions: { // options ffmpeg flags
     '-stats': '', // an option with no neccessary value uses a blank string
     '-r': 30 // options with required values specify the value after the key
